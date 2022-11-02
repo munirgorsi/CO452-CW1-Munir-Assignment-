5c59970cf3543c708faf7f7cdc7bda97d465ef4c
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-02 11:12:59 +0000

    Start working on main code

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Munir's CW1");
+        printHeading();
+        executeMenuChoice();
 
+    }
+    private static void executeMenuChoice ()
+    {
+
+        
     }
 }
