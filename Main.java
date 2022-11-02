4892fa72c594be41c2a02bd304a18cd9394e2f4f
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-02 19:19:50 +0000

    Main created

--- Main.java
+++ Main.java
@@ -2,13 +2,10 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Munir's CW1");
-        printHeading();
-        executeMenuChoice();
-
-    }
-    private static void executeMenuChoice ()
-    {
-
+        System.out.println("Well Come");
+        System.out.println("Enter your Name");
         
+
     }
+    
 }
