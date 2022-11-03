5651960d8c6cc5c7d63290a18820a80dc6473856
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-03 10:20:00 +0000

    reset main

--- Main.java
+++ Main.java
@@ -1,59 +1,8 @@
-public class Main {
+public class Main 
+{
     public static void main(String[] args)
     {
         System.out.println("Munir's CW1");
-<<<<<<< HEAD
-        printHeading();
-        executeMenuChoice();
-
     }
-    private static void executeMenuChoice ()
-    {
-        boolean appIsRunning = true;
-        while(appIsRunning == true)
-        {
-            int choice = getMenuChoice();
-            switch(choice)
-            {
-                case 1: addSong();break; 
-                case 2: deleteSong ();break;
-                case 3: printAllSongs();break;
-                case 4:appIsRunning = false;break;
-            }
-        }
 
-=======
-        System.out.println("Well Come");
-        System.out.println("Enter your Name");
->>>>>>> 4892fa72c594be41c2a02bd304a18cd9394e2f4f
-        
-
-    }
-<<<<<<< HEAD
-    private static int getMenuChoice()
-    {
-        printTitle("Top 10 Songs App");
-        int choice = 0;
-        boolean isValid = false;
-        while(isValid==false)
-        {
-            System.out.println("1 Add Song");
-            System.out.println("2 Delete Song");
-            System.out.println("3 Print all songs");
-            System.out.println("4 Print popular songs");
-            System.out.println("5 Quit");
-            choice = InpurReader.getInt("\n please enter your choice(1 to 5)>");
-            if (choice < 1|| choice > 5)
-            { 
-                System.out.println("\n invalid choice");
-
-            }
-            else isValid = true;
-        }
-        return choice;
-    }
 }
-=======
-    
-}
->>>>>>> 4892fa72c594be41c2a02bd304a18cd9394e2f4f
