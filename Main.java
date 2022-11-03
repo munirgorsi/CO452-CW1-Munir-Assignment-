4c28b35e79a6a7623080a98bef00cbcaff9ec69d
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-03 10:58:06 +0000

    construction of song 1

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main 
 {
     public static void main(String[] args)
@@ -5,8 +7,16 @@ public class Main
         System.out.println("Munir's CW1");
         System.out.println("Welcome to my Spotify clone!");
 
-        Song song1 = new Song();
-        
+        Song midnight rain = new Song(1,"Taylor swift");
+        Song bad Habit = new Song (2,"steve Lacy");
+        ArrayList<Song> songslist = new ArrayList<Song>();
+        songslist.add (midnight rain);
+        songslist.add(bad Habit);
+        for(Song current : songslist)
+        {
+            current.print();
+        }
+
     }
 
 }
--- Song.java
+++ Song.java
@@ -1,4 +1,23 @@
 public class Song 
 {
-    
+    private int id;
+    private Song name;
+    public Song (int id, Song name)
+    {
+        this.id = id;
+        this.name = Song name;
+    }
+    public void print()
+    {
+        System.out.println("id: " + this.id + "Song Name:" + this.name );
+    }
+    public int getID()
+    {
+        return this.id;
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+
 }
