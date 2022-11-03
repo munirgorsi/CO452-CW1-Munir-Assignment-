c97a337eaebc7390ab101f5b8dbc92626a4cd708
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-03 10:20:51 +0000

    greeting message

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Munir's CW1");
+        System.out.println("Welcome to my Spotify clone!");
     }
 
 }
