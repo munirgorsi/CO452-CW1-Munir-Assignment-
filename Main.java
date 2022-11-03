1c15ac0818088bc298cfc05d597fee69bfe97504
munirgorsi <115701035+munirgorsi@users.noreply.github.com>
2022-11-03 12:09:32 +0000

    added title for songs

--- Main.java
+++ Main.java
@@ -6,12 +6,18 @@ public class Main
     {
         System.out.println("Munir's CW1");
         System.out.println("Welcome to my Spotify clone!");
+        System.out.println("1:Add Song");
+        System.out.println("2: remove Song");
+        System.out.println("3: print all song list");
+        System.out.println("4: play Count");
 
-        Song midnightrain = new Song(1,"Taylor swift");
-        Song badHabit = new Song (2,"steve Lacy");
+        Song midnightrain = new Song(1,"song name","Taylor Swift");
+        Song badHabit = new Song (2, "song name", "steve Lacy");
+        Song monotonia = new Song (3, "song name ", "Shakria");
         ArrayList<Song> songslist = new ArrayList<Song>();
         songslist.add (midnightrain);
         songslist.add(badHabit);
+        songslist.add(monotonia);
         for(Song current : songslist)
         {
             current.print();
--- Song.java
+++ Song.java
@@ -1,16 +1,18 @@
 public class Song 
 {
     private int No;
+    private String title;
     private String name;
 
-    public Song (int No, String name)
+    public Song (int No, String title, String name)
     {
         this.No = No;
         this.name = name;
+        this.title = title;
     }
     public void print()
     {
-        System.out.println("No: " + this.No + "\tSong Name:" + this.name );
+        System.out.println("No: " + this.No + "Song title: " + this.title + "\tArtist Name:" + this.name );
     }
     public int getID()
     {
